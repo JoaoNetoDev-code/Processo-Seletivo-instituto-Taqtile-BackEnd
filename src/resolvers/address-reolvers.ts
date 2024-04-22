0e767375e7212b618f9144a58049c01caeac7a7e
JoaoNetoDev-code <jhontdsmit36@gmail.com>
2024-04-22 20:28:39 -0300

    feat: tentado implementar o typeDI com arquitetura mvc

--- src/resolvers/address-reolvers.ts
+++ src/resolvers/address-reolvers.ts
@@ -1,35 +1,25 @@
+import { AddressService } from './../service/address-service';
 import { Resolver, Mutation, Arg, Query, Ctx } from 'type-graphql';
-import { User } from '../entity/user';
-import { appDataSource } from '../data-source';
-import { CustomError } from '../exceptionsClass/exceptions-not-found-user';
 import { AddressModel } from '../model/address-model';
 import { CreateAddressInput } from './input-validation/address-input-validation';
-import { Address } from '../entity/address';
 import { IMyContext } from '../types/type-context';
 import jwtUtil from '../utils/jwt-util';
+import { Inject, Service } from 'typedi';
 
+@Service()
 @Resolver()
 export class AddressResolvers {
-  userServer = appDataSource.getRepository(User);
-  addressServer = appDataSource.getTreeRepository(Address);
+  private AddressService: AddressService;
+
+  constructor(@Inject() AddressService: AddressService) {
+    this.AddressService = AddressService;
+  }
 
   @Mutation(() => AddressModel)
   async createAddress(@Arg('address') address: CreateAddressInput, @Ctx() context: IMyContext): Promise<AddressModel> {
     jwtUtil.verifyToken(context.token);
 
-    const user = await this.userServer.findOne({ where: { id: address.userId } });
-
-    if (!user) {
-      throw new CustomError('Usuário não encontrado.', 404, 'Não foi possível localizar o usuário.');
-    }
-
-    const addressCreated = await this.addressServer.save(address);
-
-    user.addresses.push(addressCreated);
-
-    await this.userServer.save(user);
-
-    return { ...addressCreated, userId: user.id };
+    return this.AddressService.createAddress(address);
   }
 
   @Query(() => [AddressModel])
@@ -40,24 +30,13 @@ export class AddressResolvers {
   ): Promise<AddressModel[]> {
     jwtUtil.verifyToken(context.token);
 
-    const take = limit || 10;
-
-    return this.addressServer.find({
-      take,
-      skip: page * take,
-    });
+    return this.AddressService.getAllAddress(page, limit);
   }
 
   @Query(() => [AddressModel])
   async getAllAddressInUser(@Arg('userId') userId: number, @Ctx() context: IMyContext): Promise<AddressModel[]> {
     jwtUtil.verifyToken(context.token);
 
-    const user = await this.userServer.findOne({ where: { id: userId } });
-
-    if (!user) {
-      throw new CustomError('Usuário não encontrado.', 404, 'Não foi possível localizar o usuário.');
-    }
-
-    return user.addresses;
+    return this.AddressService.getAllAddressInUser(userId);
   }
 }
