f78df046c11c75895d2c9106063b785b259200cd
JoaoNetoDev-code <jhontdsmit36@gmail.com>
2024-04-23 18:25:56 -0300

    fix: validações redundantes removidas das migrations

--- src/migrations/1713898690839-create-user-migrate.ts
+++ src/migrations/1713898690839-create-user-migrate.ts
@@ -2,41 +2,37 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateUserMigrate1713898690839 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    const userExists = await queryRunner.hasTable('user');
-
-    if (!userExists) {
-      await queryRunner.createTable(
-        new Table({
-          name: 'user',
-          columns: [
-            {
-              name: 'id',
-              type: 'int',
-              isPrimary: true,
-              isGenerated: true,
-              generationStrategy: 'increment',
-            },
-            {
-              name: 'name',
-              type: 'varchar',
-            },
-            {
-              name: 'email',
-              type: 'varchar',
-            },
-            {
-              name: 'password',
-              type: 'varchar',
-            },
-            {
-              name: 'birthDate',
-              type: 'timestamp',
-            },
-          ],
-        }),
-        true,
-      );
-    }
+    await queryRunner.createTable(
+      new Table({
+        name: 'user',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
+            name: 'birthDate',
+            type: 'timestamp',
+          },
+        ],
+      }),
+      true,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
--- src/migrations/1713899216327-create-address-migrate.ts
+++ src/migrations/1713899216327-create-address-migrate.ts
@@ -2,65 +2,61 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateAddressMigrate1713899216327 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    const addressExists = await queryRunner.hasTable('address');
-
-    if (!addressExists) {
-      await queryRunner.createTable(
-        new Table({
-          name: 'address',
-          columns: [
-            {
-              name: 'id',
-              type: 'int',
-              isPrimary: true,
-              isGenerated: true,
-              generationStrategy: 'increment',
-            },
-            {
-              name: 'cep',
-              type: 'varchar',
-            },
-            {
-              name: 'street',
-              type: 'varchar',
-            },
-            {
-              name: 'streetNumber',
-              type: 'int',
-            },
-            {
-              name: 'complement',
-              type: 'varchar',
-            },
-            {
-              name: 'neighborhood',
-              type: 'varchar',
-            },
-            {
-              name: 'city',
-              type: 'varchar',
-            },
-            {
-              name: 'state',
-              type: 'varchar',
-            },
-            {
-              name: 'userId',
-              type: 'int',
-            },
-          ],
-          foreignKeys: [
-            {
-              columnNames: ['userId'],
-              referencedColumnNames: ['id'],
-              referencedTableName: 'user',
-              onDelete: 'CASCADE',
-            },
-          ],
-        }),
-        true,
-      );
-    }
+    await queryRunner.createTable(
+      new Table({
+        name: 'address',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'cep',
+            type: 'varchar',
+          },
+          {
+            name: 'street',
+            type: 'varchar',
+          },
+          {
+            name: 'streetNumber',
+            type: 'int',
+          },
+          {
+            name: 'complement',
+            type: 'varchar',
+          },
+          {
+            name: 'neighborhood',
+            type: 'varchar',
+          },
+          {
+            name: 'city',
+            type: 'varchar',
+          },
+          {
+            name: 'state',
+            type: 'varchar',
+          },
+          {
+            name: 'userId',
+            type: 'int',
+          },
+        ],
+        foreignKeys: [
+          {
+            columnNames: ['userId'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'user',
+            onDelete: 'CASCADE',
+          },
+        ],
+      }),
+      true,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
